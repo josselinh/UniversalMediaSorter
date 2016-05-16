54bfa18f17fd4b011f821e00917b0d2b9c6328d6
Josselin <josselin.h@gmail.com>
2016-05-17 00:54:06 +0200

    Check regex, get datetime from format, get datetime from modified date

--- UniversalMediaSorter.php
+++ UniversalMediaSorter.php
@@ -33,19 +33,128 @@ class UniversalMediaSorter
         if (empty($inputDirectory)) {
             throw new Exception('Input directory value is empty');
         }
-        
+
         /* Is a directory */
         if (!is_dir($inputDirectory)) {
             throw new Exception('Input directory is not a valid directory');
         }
-        
+
         /* Check $inputFormats */
         /* Empty */
         if (empty($inputFormats)) {
             throw new Exception('Input format value is empty');
         }
 
+        /* Open directory */
+        $handleInputDirectory = opendir($inputDirectory);
+
+        if ($handleInputDirectory) {
+            while (false !== ($entry = readdir($handleInputDirectory))) {
+                if (!in_array($entry, array('.', '..'))) {
+                    if (is_dir($inputDirectory . '/' . $entry)) {
+                        $this->findFiles($inputDirectory . '/' . $entry, $inputFormats);
+                    }
+
+                    if (is_file($inputDirectory . '/' . $entry)) {
+                        $this->analyse($inputDirectory . '/' . $entry, $inputFormats);
+                    }
+                }
+            }
+        } else {
+            throw new Exception('Input directory is not a valid directory');
+        }
+
         return $this;
+    }
+
+    /**
+     * Analyse a file
+     * @param string $inputFile
+     * @param array $inputFormats
+     */
+    private function analyse($inputFile = null, $inputFormats = array())
+    {
+        foreach ($inputFormats as $inputFormat) {
+            $inputFileMatches = array();
+            $regex = str_replace(array('%year%', '%month%', '%day%', '%hour%', '%minute%', '%second%', '%anything%'), array('(\d{4})', '(\d{2})', '(\d{2})', '(\d{2})', '(\d{2})', '(\d{2})', '(.*)'), $inputFormat);
+
+            if (preg_match('#' . $regex . '#i', $inputFile, $inputFileMatches)) {
+                $inputFormatMatches = array();
+                $inputFileFormatValues = array(
+                    'year' => null,
+                    'month' => null,
+                    'day' => null,
+                    'hour' => 0,
+                    'minute' => 0,
+                    'second' => 0);
+
+                if (preg_match_all('#' . implode('|', $this->formatKeys) . '#i', $inputFormat, $inputFormatMatches)) {
+                    foreach ($inputFormatMatches[0] as $index => $format) {
+                        $value = $inputFileMatches[$index + 1];
+
+                        if ('%year%' === $format) {
+                            if (1970 < $value && date('Y') >= $value) {
+                                $inputFileFormatValues['year'] = $value;
+                            } else {
+                                continue 2;
+                            }
+                        }
+
+                        if ('%month%' === $format) {
+                            if (1 <= $value && 12 >= $value) {
+                                $inputFileFormatValues['month'] = $value;
+                            } else {
+                                continue 2;
+                            }
+                        }
+
+                        if ('%day%' === $format) {
+                            if (1 <= $value && 31 >= $value) {
+                                $inputFileFormatValues['day'] = $value;
+                            } else {
+                                continue 2;
+                            }
+                        }
+
+                        if ('%hour%' === $format) {
+                            if (0 <= $value && 23 >= $value) {
+                                $inputFileFormatValues['hour'] = $value;
+                            } else {
+                                continue 2;
+                            }
+                        }
+
+                        if ('%minute%' === $format) {
+                            if (0 <= $value && 59 >= $value) {
+                                $inputFileFormatValues['minute'] = $value;
+                            } else {
+                                continue 2;
+                            }
+                        }
+
+                        if ('%second%' === $format) {
+                            if (0 <= $value && 59 >= $value) {
+                                $inputFileFormatValues['second'] = $value;
+                            } else {
+                                continue 2;
+                            }
+                        }
+                    }
+                }
+
+                $file = array(
+                    'filename' => $inputFile,
+                    'datetime_format' => null,
+                    'datetime_modified' => date('Y-m-d H:i:s', filemtime($inputFile)),
+                    'datetime_exif' => null);
+
+                if (!empty($inputFileFormatValues['year']) && !empty($inputFileFormatValues['month']) && !empty($inputFileFormatValues['day'])) {
+                    $file['datetime_format'] = date('Y-m-d H:i:s', mktime($inputFileFormatValues['hour'], $inputFileFormatValues['minute'], $inputFileFormatValues['second'], $inputFileFormatValues['month'], $inputFileFormatValues['day'], $inputFileFormatValues['year']));
+                }
+
+                $this->files[] = $file;
+            }
+        }
     }
 
     /**
@@ -55,94 +164,6 @@ class UniversalMediaSorter
     public function getFiles()
     {
         return $this->files;
-    }
-
-    /**
-     * Contains regex to find files
-     * @var array
-     */
-    private $oldregex = array();
-
-    /**
-     * Convert a string to a regex and save positions
-     * @example DD-MM-YYYY_HH-MM-SS_X.jpg => (\d{2})-(\d{2})-(\d{4})_(\d{2})-(\d{2})-(\d{2})_(.*).jpg
-     * @param string $string
-     */
-    public function oldsetRegexByString($string)
-    {
-        $matches = array();
-        $search = array('%year%', '%month%', '%day%', '%hour%', '%minute%', '%second%', '%anything%');
-        $replace = array('(\d{4})', '(\d{2})', '(\d{2})', '(\d{2})', '(\d{2})', '(\d{2})', '(.*)');
-        $regex['regex'] = str_replace($search, $replace, $string);
-
-        if (preg_match_all('#' . implode('|', $search) . '#i', $string, $matches)) {
-            $regex['index'] = array_flip($matches[0]);
-        }
-
-        $this->regex[] = $regex;
-
-        pr($regex);
-        die;
-    }
-
-    /**
-     * Browse input directory and returns files with datetime using previous regex
-     * @param string $input directory
-     * @return array Files with datetimes
-     */
-    public function oldgetFiles($input = null, &$filesDatetimes = array())
-    {
-        if (is_dir($input)) {
-            $handleDirectory = opendir($input);
-
-            if ($handleDirectory) {
-                while (false !== ($entry = readdir($handleDirectory))) {
-                    if (!in_array($entry, array('.', '..'))) {
-                        if (is_dir($input . DS . $entry)) {
-                            $this->getFiles($input . DS . $entry);
-                        }
-
-                        if (is_file($input . DS . $entry)) {
-                            $fileDatetimes = $this->fileMatchesRegex($input . DS . $entry);
-
-                            if (false !== $fileDatetimes) {
-                                $filesDatetimes[] = $fileDatetimes;
-                            }
-                        }
-                    }
-                }
-
-                return $filesDatetimes;
-            } else {
-                throw new Exception('"' . $input . '" is not a valid directory');
-            }
-        } else {
-            throw new Exception('"' . $input . '" is not a directory');
-        }
-    }
-
-    private function oldfileMatchesRegex($file = null)
-    {
-        foreach ($this->regex as $regex) {
-            $matches = array();
-
-            if (preg_match('#' . $regex['regex'] . '#i', $file, $matches)) {
-                $formatMatches = array();
-
-                foreach ($regex['index'] as $format => $position) {
-                    $formatMatches[$format] = $matches[$position + 1];
-                }
-
-                $fileDatetimes = array(
-                    'file' => $file,
-                    'datetime' => null
-                );
-
-                return $fileDatetimes;
-            }
-        }
-
-        return false;
     }
 
 }
