15b174539886e6e58edeb015ef4a6baff63d2ab7
Josselin <josselin.h@gmail.com>
2016-05-24 21:54:54 +0200

    Just do some CSS

--- step-1.php
+++ step-1.php
@@ -10,10 +10,10 @@
 
     <body>
         <form method="post" action="step-2.php">
-            <div class="form-section">
-                <span class="form-section-title">Input</span>
+            <div class="section">
+                <span class="title">Input</span>
 
-                <div class="form-section-content">
+                <div class="content">
                     <div>
                         <label>Directory to analyse :
                             <input type="text" name="data[input][directory]" placeholder="Directory to analyse" value="/home/josselin/Projects/UniversalMediaSorter/Unsorted"/>
@@ -21,26 +21,23 @@
                     </div>
 
                     <div>
-                        <fieldset>
-                            <legend>Formats</legend>
-
-                            <?php foreach ($inputFormats as $inputFormat): ?>
-                                <div>
-                                    <label>
-                                        <input type="checkbox" name="data[input][formats][]" value="<?php echo $inputFormat; ?>" checked/>
-                                        <?php echo $inputFormat; ?>
-                                    </label>
-                                </div>
-                            <?php endforeach; ?>
-                        </fieldset>
+                        Formats :
+                        <?php foreach ($inputFormats as $inputFormat): ?>
+                            <div>
+                                <label>
+                                    <input type="checkbox" name="data[input][formats][]" value="<?php echo $inputFormat; ?>" checked/>
+                                    <?php echo $inputFormat; ?>
+                                </label>
+                            </div>
+                        <?php endforeach; ?>
                     </div>
                 </div>
             </div>
 
-            <div class="form-section">
-                <span class="form-section-title">Output</span>
+            <div class="section">
+                <span class="title">Output</span>
 
-                <div class="form-section-content">
+                <div class="content">
                     <div>
                         <label>Destination directory :
                             <input type="text" name="data[output][directory]" placeholder="Output directory" value="/home/josselin/Projects/UniversalMediaSorter/Sorted"/>
@@ -54,26 +51,28 @@
                             </label>
                         </div>
                     <?php endforeach; ?>
-                    
+
                     <div>
                         Existing file :
-                        <label>
-                            <input type="radio" name="data[output][options][existing]" value="rename" checked/>
-                            Rename
-                        </label>
-                        <label>
-                            <input type="radio" name="data[output][options][existing]" value="overwrite"/>
-                            Overwrite
-                        </label>
-                        <label>
-                            <input type="radio" name="data[output][options][existing]" value="ignore"/>
-                            Ignore
-                        </label>
+                        <div>
+                            <label>
+                                <input type="radio" name="data[output][options][existing]" value="rename" checked/>
+                                Rename
+                            </label>
+                            <label>
+                                <input type="radio" name="data[output][options][existing]" value="overwrite"/>
+                                Overwrite
+                            </label>
+                            <label>
+                                <input type="radio" name="data[output][options][existing]" value="ignore"/>
+                                Ignore
+                            </label>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="form-submit">
+            <div class="section">
                 <button type="submit" name="data[submit]" value="analyse">Analyse</button>
             </div>
         </form>
--- step-2.php
+++ step-2.php
@@ -11,45 +11,47 @@
     <body>        
         <?php if (!empty($files)) : ?>
             <form method="post" action="step-3.php">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Filename</th>
-                            <?php foreach ($datetime_titles as $datetime_title): ?>
-                                <th><?php echo $datetime_title; ?></th>
-                            <?php endforeach; ?>
-                            <!--th>Other</th-->
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        <?php foreach ($files as $i => $file): ?>
+                <div class="section">
+                    <table>
+                        <thead>
                             <tr>
-                                <td>
-                                    <?php echo sprintf('<input type="hidden" name="data[input][%d][filename]" value="%s"/>%s', $i, $file['filename'], basename($file['filename'])); ?>
-                                </td>
-
-                                <?php
-                                $min = minNotNull($file['datetime']);
-
-                                foreach ($file['datetime'] as $datetime) {
-                                    echo '<td>';
-
-                                    if (!empty($datetime)) {
-                                        echo sprintf('<label><input type="radio" name="data[input][%d][datetime]" value="%s" %s/>%s</label>', $i, $datetime, ($min === $datetime ? 'checked' : ''), date('Y-m-d H:i:s', $datetime));
-                                    }
-
-                                    echo '</td>';
-                                }
-                                ?>
-
-                                <!--td>
-                                <?php echo sprintf('<input type="radio" name="data[input][%d][datetime]" value="other"/><input type="text" name="data[input][%d][other]"/>', $i, $i); ?>
-                                </td-->
+                                <th>Filename</th>
+                                <?php foreach ($datetime_titles as $datetime_title): ?>
+                                    <th><?php echo $datetime_title; ?></th>
+                                <?php endforeach; ?>
+                                <!--th>Other</th-->
                             </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+                        </thead>
+
+                        <tbody>
+                            <?php foreach ($files as $i => $file): ?>
+                                <tr>
+                                    <td>
+                                        <?php echo sprintf('<input type="hidden" name="data[input][%d][filename]" value="%s"/>%s', $i, $file['filename'], basename($file['filename'])); ?>
+                                    </td>
+
+                                    <?php
+                                    $min = minNotNull($file['datetime']);
+
+                                    foreach ($file['datetime'] as $datetime) {
+                                        echo '<td>';
+
+                                        if (!empty($datetime)) {
+                                            echo sprintf('<label><input type="radio" name="data[input][%d][datetime]" value="%s" %s/>%s</label>', $i, $datetime, ($min === $datetime ? 'checked' : ''), date('Y-m-d H:i:s', $datetime));
+                                        }
+
+                                        echo '</td>';
+                                    }
+                                    ?>
+
+                                    <!--td>
+                                    <?php echo sprintf('<input type="radio" name="data[input][%d][datetime]" value="other"/><input type="text" name="data[input][%d][other]"/>', $i, $i); ?>
+                                    </td-->
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
 
                 <input type="hidden" name="data[output][directory]" value="<?php echo $outputDirectory; ?>"/>
                 <?php foreach ($outputFormats as $ext => $outputFormat): ?>
@@ -59,7 +61,7 @@
                     <input type="hidden" name="data[output][options][<?php echo $key; ?>]" value="<?php echo $outputOption; ?>"/>
                 <?php endforeach; ?>
 
-                <div>
+                <div class="section">
                     <button type="submit" name="submit" value="sort">Sort</button>
                 </div>
             </form>
